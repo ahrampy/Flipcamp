5ed15a2df1810a01c6f0880c235a8991842a25ee
Adrian Rampy <ahrampy@gmail.com>
2019-11-06 09:43:19 -0800

    Resize demo button

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -7,7 +7,7 @@ class Api::SessionsController < ApplicationController
             )
         if @user
             signin(@user)
-            render json: @user
+            render "api/users/show"
         else
             render json: ["Invalid email or password"], status: 401
         end
