fc15e76ef2aaf4af44131f9937b9b78cb016af9e
your name here <ahrampy@gmail.com>
2019-11-12 23:00:59 -0800

    Add h1 on splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 import SiteIndex from '../sites/sites_index_contain';
 
 const Splash = () => (
-    <div className="splash-main">
-        <div className='splash-site-index'>
-            <SiteIndex/>
+    <div className="splash-main-container">
+        <div className="splash-main">
+            <div className='splash-header'>
+                <h1>
+                    Natural discovery.
+                </h1>
+                <p>
+                    Book unique camping experiences on over 300,000 campsites, cabins, RV parks, public parks and more.
+                </p>
+            </div>
+            <div className='splash-site-index'>
+                <SiteIndex/>
+            </div>
         </div>
     </div>
 )
