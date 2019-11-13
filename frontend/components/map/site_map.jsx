ac8f46346c687b46ec800cbdf20c98b1b574ad03
Adrian Rampy <ahrampy@gmail.com>
2019-11-13 10:07:36 -0800

    Init site map

--- frontend/components/map/site_map.jsx
+++ frontend/components/map/site_map.jsx
@@ -7,19 +7,18 @@ class SiteMap extends React.Component {
     }
 
     componentDidMount() {
-        // set the map to show SF
+
         const mapOptions = {
-            center: { lat: 37.7758, lng: -122.435 }, // this is SF
-            zoom: 13
+            center: { lat: this.props.lat, lng: this.props.lng },
+            zoom: 10
         };
 
-        // wrap this.mapNode in a Google Map
         this.map = new google.maps.Map(this.mapNode, mapOptions);
     }
 
     render() {
         return (
-            <div ref={map => this.mapNode = map}>
+            <div id='widget-map' ref={map => this.mapNode = map}>
 
             </div>
         )
