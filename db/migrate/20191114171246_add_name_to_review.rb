b764e942bbb21813d36dc94d9b28ed1f017594f3
Adrian Rampy <ahrampy@gmail.com>
2019-11-14 09:15:06 -0800

    Add author reviews

---
+++ db/migrate/20191114171246_add_name_to_review.rb
@@ -0,0 +1,5 @@
+class AddNameToReview < ActiveRecord::Migration[5.2]
+  def change
+    add_column :reviews, :author, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_004341) do
+ActiveRecord::Schema.define(version: 2019_11_14_171246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_11_14_004341) do
     t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
   create_table "sites", force: :cascade do |t|
