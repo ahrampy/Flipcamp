22d6b71bd97754cd6ecc20cdbf300a00a3c4a7e0
your name here <ahrampy@gmail.com>
2019-11-15 11:05:52 -0800

    Change circle marker

--- frontend/components/map/site_map.jsx
+++ frontend/components/map/site_map.jsx
@@ -15,16 +15,27 @@ class SiteMap extends React.Component {
 
         this.map = new google.maps.Map(this.mapNode, mapOptions);
 
-        this.iconBase = 'https://maps.google.com/mapfiles/kml/shapes/'
+        // this.iconBase = 'https://maps.google.com/mapfiles/kml/shapes/'
 
-        this.marker = new google.maps.Marker({
-            position: { lat: this.props.lat, lng: this.props.lng },
-            icon: {
-                url: "http://maps.google.com/mapfiles/ms/icons/orange.png"
-            }
-        });
+        // this.marker = new google.maps.Marker({
+        //     position: { lat: this.props.lat, lng: this.props.lng },
+        //     icon: {
+        //         url: "http://maps.google.com/mapfiles/ms/icons/orange.png"
+        //     }
+        // });
 
-        this.marker.setMap(this.map);
+        // this.marker.setMap(this.map);
+
+        this.circle = new google.maps.Circle({
+            strokeColor: '#F6C270',
+            strokeOpacity: 0.8,
+            strokeWeight: 2.5,
+            fillColor: '#F6C270',
+            fillOpacity: 0.40,
+            map: this.map,
+            center: { lat: this.props.lat, lng: this.props.lng },
+            radius: 2000
+        })
     }
 
     render() {
