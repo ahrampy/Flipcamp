bd8b0597298a2992cb126ad7e0315fe8ba295bf2
your name here <ahrampy@gmail.com>
2020-02-04 17:15:38 -0800

    Add index filter search

--- frontend/components/sites/sites_index_pres.jsx
+++ frontend/components/sites/sites_index_pres.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SiteTile from './sites_index_tile';
 
 class SiteIndex extends React.Component {
     constructor (props) {
         super(props);
+        this.state = {
+            searchParams: '',
+            searching: false
+        }
+        this.handleSearch = this.handleSearch.bind(this);
     }
 
     componentDidMount() {
         this.props.fetchSites();
         this.props.fetchBookings();
         window.scrollTo(0, 0);
+    }
+
+    handleSearch(event) {
+        this.setState({ searchParams: event.target.value.toLowerCase() });
+        if (this.state.searchParams !== '') {
+            this.state.searching = true;
+        } else {
+            this.state.searching = false;
+        }
     }
 
     render () {
@@ -20,22 +33,39 @@ class SiteIndex extends React.Component {
         }
 
         return (
-            <div className='index-sites'> {
-                this.props.sites.map(site => {
-                    let booked = false;
-                    this.props.bookings.forEach(booking => {
-                        if (this.props.currentUser) {
-                            if (booking.site_id === site.id &&
-                                booking.user_id === this.props.currentUser.id) {
-                                booked = true;
+            <div>
+                <div>
+                    <input
+                        type="text"
+                        value={this.state.searchParams}
+                        placeholder="Search for campsites..."
+                        onChange={this.handleSearch}
+                    />
+                </div>
+                <div className='index-sites'> {
+                    this.props.sites.map(site => {
+                        if (this.state.searching) {
+                            if (!site.title.toLowerCase().includes(this.state.searchParams) &&
+                                !site.site_type.toLowerCase().includes(this.state.searchParams)
+                            ) {
+                              return
                             }
                         }
+                        let booked = false;
+                        this.props.bookings.forEach(booking => {
+                            if (this.props.currentUser) {
+                                if (booking.site_id === site.id &&
+                                    booking.user_id === this.props.currentUser.id) {
+                                    booked = true;
+                                }
+                            }
+                        })
+                        return (
+                            <SiteTile key={site.id} site={site} booked={booked}/>
+                        )
                     })
-                    return (
-                        <SiteTile key={site.id} site={site} booked={booked}/>
-                    )
-                })
-            }
+                }
+                </div>
             </div>
         )
     };
