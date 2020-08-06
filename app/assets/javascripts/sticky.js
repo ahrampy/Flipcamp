12a5975bc2caafb70816021919fa1d62c0868559
adrian_rampy <ahrampy@gmail.com>
2020-08-05 22:21:24 -0700

    add map height based on window

--- app/assets/javascripts/sticky.js
+++ app/assets/javascripts/sticky.js
@@ -1,20 +1,27 @@
-var dist;
+var distTop;
 
-$(window).load(function(e) {
-  dist = $("#site-show-widget-container").offset().top;
-})
+// function setSize () {
 
-$(window).resize(function (e) {
-  dist = $("#site-show-widget-container").offset().top;
-})
+// }
+
+$(window).on("load resize", function (e) {
+  distTop = $("#site-show-widget-container").offset().top;
+  mapSize =
+    $(window).height() - $("#site-show-widget-container").height() - 135;
+  $(".site-show-widget-map-container").css({ height: mapSize });
+});
+
+// $(window).resize(function (e) {
+//   distTop = $("#site-show-widget-container").offset().top;
+// });
 
 $(window).scroll(function (e) {
   var $bookWidget = $("#site-show-widget-container");
   var isPositionFixed = $bookWidget.css("position") == "fixed";
-  if ($(this).scrollTop() > dist && !isPositionFixed) {
+  if ($(this).scrollTop() > distTop && !isPositionFixed) {
     $bookWidget.css({ position: "fixed", top: "75px", bottom: "" });
   }
-  if ($(this).scrollTop() < dist && isPositionFixed) {
+  if ($(this).scrollTop() < distTop && isPositionFixed) {
     $bookWidget.css({ position: "absolute", top: "", bottom: "0px" });
   }
 });
